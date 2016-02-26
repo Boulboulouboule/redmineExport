c50c8c89edfa024718ec611beaa42044bb4cca60
Boulboulouboule <vpierrat@actency.local>
2016-02-26 17:22:26 +0100

    Classement

--- RedmineExport.php
+++ RedmineExport.php
@@ -57,13 +57,19 @@ class RedmineExport{
       $issues = [];
       foreach($data as $key=>$issue){
         if ($issue->root_id != $data[$key-1]->root_id) {
-          $issues[$issue->root_id];
+          $issues[$issue->root_id][] = $issue;
         }
       }
 
       foreach($data as $key=>$issue){
-        if ($issue->root_id && $issue->parent_id && $issue->parent_id != $data[$key-1]->parent_id) {
-          $issues[$issue->root_id] .= $issue->parent_id;
+        if ($issue->root_id && $issue->parent_id && $issue->parent_id != $data[$key-1]->parent_id && !in_array($issue->parent_id, $issues)) {
+          $issues[$issue->root_id][$issue->parent_id][] = $issue;
+        }
+      }
+
+      foreach($data as $key=>$issue){
+        if ($issue->parent_id == $issues[$issue->root_id][$issue->parent_id] || $issue->id == $issues[$issue->root_id][$issue->parent_id]) {
+          $issues[$issue->root_id][$issue->parent_id][$issue->id][] = $issue;
         }
       }
       var_dump($issues);
